6ed98b8fee322d68e135ee8b8e96dce50619b154
PriyanakaBhioade <bhiogadePriyanak93@gmail.com>
2019-12-27 22:04:52 +0530

    Added:Code To check PIN Code is valid or not

--- pattern.js
+++ pattern.js
@@ -1,2 +1,9 @@
-console.log("Pattern Program");
+let read = require("readline-sync");
+let pinCode = read.question("Enter your pin code : ");
+let pinCodeRegex = RegExp('^[0-9]{6}$')
+let pinCodeCheck = pinCodeRegex.test(pinCode);
+if(pinCodeCheck == true)
+	console.log(pinCode ,"is Valid");
+else
+        console.log(pinCode ,"is Invalid");
 
