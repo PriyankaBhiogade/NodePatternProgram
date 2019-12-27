3b6dc7d420e767e3646ca12bff43f4d387038dda
PriyanakaBhioade <bhiogadePriyanak93@gmail.com>
2019-12-27 22:44:29 +0530

    Refactor: Code Created Function For checking PinCode

--- pattern.js
+++ pattern.js
@@ -1,5 +1,7 @@
 let read = require("readline-sync");
 let pinCode = read.question("Enter your pin code : ");
+
+function checkPinCode(pinCode){
 let pinCodeRegex = RegExp('^[0-9]{3} [0-9]{3}$')
 let pinCodeCheck = pinCodeRegex.test(pinCode);
 if(pinCodeCheck == true)
@@ -7,3 +9,5 @@ if(pinCodeCheck == true)
 else
         console.log(pinCode ,"is Invalid Pin Code");
 
+}
+checkPinCode(pinCode);
