15d27fbe169197785199a41f5df5b52ac5ce2b74
PriyanakaBhioade <bhiogadePriyanak93@gmail.com>
2019-12-27 23:03:59 +0530

    Added: Function To check Email is vaild Or Not

--- pattern.js
+++ pattern.js
@@ -1,5 +1,6 @@
 let read = require("readline-sync");
 let pinCode = read.question("Enter your pin code : ");
+let emailPattern = read.question("Enter your Email : ");
 
 function checkPinCode(pinCode){
 let pinCodeRegex = RegExp('^[0-9]{3} [0-9]{3}$')
@@ -10,4 +11,16 @@ else
         console.log(pinCode ,"is Invalid Pin Code");
 
 }
+
+function checkEmail(emailPattern){
+let emailRegex = RegExp('[a-zA-Z0-9]([_,+,-,.]{0,1}[a-zA-Z0-9])*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,3}([.]{1}[a-zA-Z]{2,3}){0,1}$');
+let emailCheck = emailRegex.test(emailPattern);
+if(emailCheck == true)
+        console.log(emailPattern ,"is Valid Email ");
+else
+        console.log(emailPattern ,"is Invalid Email ");
+
+}
+
 checkPinCode(pinCode);
+checkEmail(emailPattern);
